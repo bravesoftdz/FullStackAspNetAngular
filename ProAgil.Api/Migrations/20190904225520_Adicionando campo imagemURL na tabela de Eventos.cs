4c178eb94a66726fdfbfa2f977986222ab6e798c
Camila Nunes <cvn.camila@gmail.com>
2019-09-13 23:34:20 -0300

    Criação da API #AB#98;

---
+++ ProAgil.Api/Migrations/20190904225520_Adicionando campo imagemURL na tabela de Eventos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.Api.Migrations
+{
+    public partial class AdicionandocampoimagemURLnatabeladeEventos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Eventos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Eventos");
+        }
+    }
+}
